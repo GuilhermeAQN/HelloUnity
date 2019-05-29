78eb8de09c4cbbb799d6441fbdd37529e1c40a07
GuilhermeAQN <guilhermeaqn92@gmail.com>
2019-05-29 11:14:34 -0400

    Attack projectile and Enemie dance

--- EstudoUnity/Assets/Scripts/PlayerController.cs
+++ EstudoUnity/Assets/Scripts/PlayerController.cs
@@ -16,23 +16,41 @@ public class PlayerController : MonoBehaviour
     public float timeInvincible = 2.0f;
     float invincibleTimer;
 
+    Animator anim;
+    Vector2 lookDirection = new Vector2(0,-1);
+    
+    public GameObject projectilePrefab;
+
     void Start(){
         rb2D = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+
+        anim = GetComponent<Animator>();
     }
 
     void Update(){
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector2 position = transform.position;
+        
+        Vector2 move = new Vector2(horizontal, vertical);
 
-        if(Mathf.Abs(horizontal) + Mathf.Abs(vertical) == 2){
-            position.x = position.x + speed / Mathf.Sqrt(2) * horizontal * Time.deltaTime;
-            position.y = position.y + speed / Mathf.Sqrt(2) * vertical * Time.deltaTime;
-        }else{
-            position.x = position.x + speed * horizontal * Time.deltaTime;
-            position.y = position.y + speed * vertical * Time.deltaTime;
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)){
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
         }
+
+        anim.SetFloat("Look X", lookDirection.x);
+        anim.SetFloat("Look Y", lookDirection.y);
+        anim.SetFloat("Speed", move.magnitude);
+
+        Vector2 position = rb2D.position;
+        
+        if(Mathf.Abs(horizontal) + Mathf.Abs(vertical) == 2){
+            position = position + move * speed / Mathf.Sqrt(2) * Time.deltaTime;
+        }else{
+            position = position + move * speed * Time.deltaTime;
+        }
+        Debug.Log(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
         rb2D.MovePosition(position);
 
         if(isInvincible){
@@ -40,6 +58,11 @@ public class PlayerController : MonoBehaviour
             if(invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        if(Input.GetKeyDown(KeyCode.C)){
+            Launch();
+        }
+        
     }
     
     public void ChangeHealth(int amount){
@@ -47,10 +70,21 @@ public class PlayerController : MonoBehaviour
             if(isInvincible)
                 return;
 
+            anim.SetTrigger("Hit");
             isInvincible = true;
             invincibleTimer = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    
+    void Launch(){
+        GameObject projectileObject = Instantiate(projectilePrefab, rb2D.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        anim.SetTrigger("Launch");
     }
 }
---
+++ EstudoUnity/Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+
+    void Awake(){
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+        if(transform.position.magnitude > 50.0f){
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force){
+        rb2D.AddForce(direction * force);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        EnemieController e = other.gameObject.GetComponent<EnemieController>();
+        if(e != null){
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+}
