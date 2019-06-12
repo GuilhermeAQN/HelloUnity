3d7217f997cd0ecc75e6bfbc30ed6b28a05ecb9e
GuilhermeAQN <guilhermeaqn92@gmail.com>
2019-06-11 21:24:18 -0400

    Platform 2d controller

---
+++ ControllerTest/Assets/Collisions.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Collisions : MonoBehaviour{
+
+    public LayerMask groundMask;
+
+    private BoxCollider2D coll;
+    private PlayerController player;
+    private RaycastOrigin raycastOrigin;
+
+    float rayDis = 0.05f;
+
+    const float skinWidth = .015f;
+    public bool onGround;
+    public bool onWall;
+    public bool onRightWall;
+    public bool onLeftWall;
+    
+    public int wallSide;
+
+    void Awake(){
+        coll = GetComponent<BoxCollider2D>();
+        player = GetComponent<PlayerController>();
+    }
+
+    void FixedUpdate(){
+        UpdateRayOrigin();
+        UpdateCollision();
+    }
+
+    void UpdateCollision(){
+
+    //--------------Horizontal Collisions--------------//
+        float direction = player.dirX;
+
+        Vector2 rayTopOrigin = (direction == -1)? raycastOrigin.topLeft : raycastOrigin.topRight;
+        Vector2 rayBotOrigin = (direction == -1)? raycastOrigin.bottomLeft : raycastOrigin.bottomRight;
+
+        Vector2 rayDirection = (direction == -1)? Vector2.right : Vector2.left;
+        
+        RaycastHit2D topMaxRay = Physics2D.Raycast(rayTopOrigin, rayDirection * -1, rayDis, groundMask);
+        RaycastHit2D topMinRay = Physics2D.Raycast(rayBotOrigin, rayDirection * -1, rayDis, groundMask);
+
+        // Debug.DrawRay(rayTopOrigin, rayDirection * -rayDis, Color.red);
+        // Debug.DrawRay(rayBotOrigin, rayDirection * -rayDis, Color.red);
+
+        if(topMaxRay || topMinRay)
+            onWall = true;
+        else
+            onWall = false;
+
+        if(direction == 1 && onWall)
+            onRightWall = true;
+        else
+            onRightWall = false;
+
+        if(direction == -1 && onWall)
+            onLeftWall = true;
+        else
+            onLeftWall = false;
+
+        wallSide = onRightWall ? -1 : 1;
+
+    //--------------Vertical Collisions--------------//
+        RaycastHit2D botLeftRay = Physics2D.Raycast(raycastOrigin.bottomLeft, Vector2.up * -1, rayDis, groundMask);
+        RaycastHit2D botRightRay = Physics2D.Raycast(raycastOrigin.bottomRight, Vector2.up * -1, rayDis, groundMask);
+
+        // Debug.DrawRay(raycastOrigin.bottomLeft, Vector2.up * -rayDis, Color.red);
+        // Debug.DrawRay(raycastOrigin.bottomRight, Vector2.up * -rayDis, Color.red);
+            
+        if(botLeftRay || botRightRay){
+            onGround = true;
+        }else{
+            onGround = false;
+        }
+    }
+
+    public void UpdateRayOrigin(){
+        Bounds bounds = coll.bounds;
+        bounds.Expand (skinWidth * -.5f);
+        
+        raycastOrigin.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
+        raycastOrigin.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
+        raycastOrigin.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+        raycastOrigin.topRight = new Vector2 (bounds.max.x, bounds.max.y);
+    }
+
+    struct RaycastOrigin{
+        public Vector2 bottomLeft, bottomRight;
+        public Vector2 topLeft, topRight;
+    }
+}
--- ControllerTest/Assets/PlayerController.cs
+++ ControllerTest/Assets/PlayerController.cs
@@ -5,37 +5,143 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
 
-    private Rigidbody2D rb;
+    private Animations anim;
+    private Collisions coll;
+    [HideInInspector]
+    public Rigidbody2D rb;
+
     public float speed = 10;
     public float jumpForce = 5;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
 
-    void Start()
-    {
+    public float slideSpeed = 2;
+    public float wallJumpLerp = 10;
+    
+    [HideInInspector]
+    public float dirX = 1;
+
+    public bool canMove;
+    public bool wallJumped;
+    public bool wallSlide;
+    public bool jumping;
+
+    private Vector2 dir;
+
+    void Start(){
+        coll = GetComponent<Collisions>();
+        anim = GetComponent<Animations>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
+    void Update(){
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector2 dir = new Vector2(x, y);
 
+        if(x > 0)
+            dirX = 1;
+        else if(x < 0)
+            dirX = -1;
+
         Walk(dir);
-        
-        if(Input.GetButtonDown("Jump"))
-            Jump(Vector2.up, false);
+
+        //----------Call Wall Slide----------//
+        if(coll.onWall && !coll.onGround){
+            if(x != 0 && !jumping){
+                wallSlide = true;
+                WallSlide();
+            }
+            if(x != 0 && jumping){
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+        }
+        if(coll.onGround){
+            wallSlide = false;
+            wallJumped = false;
+        }
+
+        //----------Call Jump and Wall Jump----------
+        if(Input.GetButtonDown("Jump")){
+            // anim.SetTrigger("jump");
+
+            if(coll.onGround){
+                Jump(Vector2.up, false);
+                jumping = true;
+            }
+            if(coll.onWall && !coll.onGround)
+                WallJump();
+        }
+
+        //----------Jump Sustain Code----------//
+        if(rb.velocity.y < 0){
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if(rb.velocity.y > 0 && !Input.GetButton("Jump")){
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+        if (rb.velocity.y < 0)
+            jumping = false;
     }
 
-    private void Walk(Vector2 dir)
-    {
-        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+    void Walk(Vector2 dir){
+        if(!canMove)
+            return;
+
+        if(coll.onWall){
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+            return;
+        }
+
+        if(!wallJumped){
+            rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+        }else{
+            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+        }
     }
 
-    private void Jump(Vector2 dir, bool wall)
-    {
+    void Jump(Vector2 dir, bool wall){
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.velocity += dir * jumpForce;
     }
 
-    
+    private void WallJump(){
+        if((dirX == 1 && coll.onRightWall) || dirX == -1 && coll.onLeftWall){
+            dirX *= -1;
+            anim.Flip(dirX);
+        }
+
+        StopCoroutine(DisableMovement(0));
+        StartCoroutine(DisableMovement(.15f));
+
+        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
+
+        Jump((Vector2.up / 1.3f + wallDir / 1.3f), true);
+
+        wallJumped = true;
+    }
+
+
+    void WallSlide(){
+        if(coll.wallSide != dirX)
+            anim.Flip(dirX * -1);
+        
+        if(!canMove)
+            return;
+        
+        bool pushingWall = false;
+        if((rb.velocity.x > 0 && coll.onRightWall) || (rb.velocity.x < 0 && coll.onLeftWall) && !jumping){
+            pushingWall = true;
+        }
+        float push = pushingWall ? 0 : rb.velocity.x;
+
+        if(!jumping)
+            rb.velocity = new Vector2(push, -slideSpeed);
+    }
+
+    IEnumerator DisableMovement(float time){
+        canMove = false;
+        yield return new WaitForSeconds(time);
+        canMove = true;
+    }
 }
