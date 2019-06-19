6cc30be62552d5dc9fba5cbfe53c2bfa25a04f66
GuilhermeAQN <guilhermeaqn92@gmail.com>
2019-06-19 14:23:44 -0400

    test

---
+++ ControllerTest/Assets/Scripts/Animations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour{
+
+    private Animator anim;
+    private PlayerController player;
+    private Collisions coll;
+    [HideInInspector]
+    public SpriteRenderer sr;
+
+    void Start(){
+        anim = GetComponent<Animator>();
+        player = GetComponent<PlayerController>();
+        coll = GetComponent<Collisions>();
+        sr = GetComponent<SpriteRenderer>();
+    } 
+
+    void Update(){
+        
+    }
+
+    public void Flip(float dirX){
+        if(player.wallSlide){
+            if(dirX == -1 && sr.flipX)
+                return;
+
+            if(dirX == 1 && !sr.flipX)
+                return;
+        }
+
+        bool side = (dirX == 1) ? false : true;
+        sr.flipX = side;
+    }
+}
