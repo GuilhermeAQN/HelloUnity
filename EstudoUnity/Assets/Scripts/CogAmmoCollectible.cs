50b73b4cb422f247a33e3647b3b1cfd197076095
GuilhermeAQN <guilhermeaqn92@gmail.com>
2019-06-27 13:43:25 -0400

    Ammo Collect Particle

--- EstudoUnity/Assets/Scripts/CogAmmoCollectible.cs
+++ EstudoUnity/Assets/Scripts/CogAmmoCollectible.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CogAmmoCollectible : MonoBehaviour
-{
+public class CogAmmoCollectible : MonoBehaviour{
+    public ParticleSystem polish;
+    
     void OnTriggerEnter2D(Collider2D other){
         PlayerController playerController = other.GetComponent<PlayerController>();
 
         if(playerController != null){
             if(CogAmmoControll.instence.currentCogAmmo < CogAmmoControll.instence.maxCogAmmo){
                 CogAmmoControll.instence.PickUpAmmo(Random.Range(1, 5));
+                Instantiate(polish, transform.position, transform.rotation);
                 Destroy(gameObject);
             }
         }
-
     }
 }
