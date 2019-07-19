60758070a7df42e353d7ba0563951e2c3b26ed34
GuilhermeAQN <guilhermeaqn92@gmail.com>
2019-07-19 17:47:25 -0400

    Atividade 16

--- BeginnerScripting/Assets/Scripts/16. GetButton and GetKey/KeyInput.cs
+++ BeginnerScripting/Assets/Scripts/16. GetButton and GetKey/KeyInput.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class KeyInput : MonoBehaviour{
 
     Image img;
+
+    public TextMeshProUGUI text;
+
+    public Color standard;
+    public Color downgfx;
+    public Color upgfx;
+    public Color heldgfx;
 
     private void Start() {
         img = GetComponent<Image>();
@@ -17,7 +25,21 @@ public class KeyInput : MonoBehaviour{
         bool up = Input.GetKeyUp(KeyCode.Space);
 
         if(down){
-
+            img.color = downgfx;
+            Debug.Log("Down");
         }
+        else if(held){
+            img.color = heldgfx;
+            Debug.Log("Held");
+        }
+        else if(up){
+            img.color = upgfx;
+            Debug.Log("Up");
+        }
+        else{
+            img.color = standard;
+        }
+
+        text.text = down + "\n\n" + held + "\n\n" + up;
     }
 }
